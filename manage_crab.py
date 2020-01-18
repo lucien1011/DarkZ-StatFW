e64f22b9c29d3f14f8f8ad966680f7f734165769
lucien1011 <lucien1011@gmail.com>
2020-01-18 08:20:42 -0500

    updater manage_crab.py

--- manage_crab.py
+++ manage_crab.py
@@ -1,6 +1,8 @@
 import os,copy,math,argparse,glob
 from Utilities.mkdir_p import mkdir_p
 from CRABAPI.RawCommand import crabCommand
+from CRABClient.ClientExceptions import CachefileNotFoundException
+from httplib import HTTPException
 
 parser = argparse.ArgumentParser()
 #parser.add_argument("--inputDir",action="store")
@@ -15,14 +17,24 @@ option = parser.parse_args()
 
 for crabDir in glob.glob(option.pattern):
     if option.verbose:
-        print "********************"
+        print "*"*100
         print "Running on directory "+crabDir
     try:
         status_res = crabCommand('status',dir=crabDir)
     except HTTPException as ex:
         print("Problem with status encountered: %s" % ex)
-        raise
+    except CachefileNotFoundException as ex:
+        print("Problem with status encountered: %s" % ex)
+
     if status_res["dagStatus"] == "COMPLETED" and option.purge:
         if option.verbose:
             print "Purging directory "+crabDir
-        res = crabCommand('purge',dir=crabDir)
+        try:
+            res = crabCommand('purge',dir=crabDir)
+        except HTTPException as ex:
+            print("Problem with purge encountered: %s" % ex)
+        except CachefileNotFoundException as ex:
+            print("Problem with status encountered: %s" % ex) 
+
+    if option.verbose:
+        print "*"*100
