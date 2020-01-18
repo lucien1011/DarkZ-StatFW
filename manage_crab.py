cd5b1a2d9898eb34294a809b7526dfceb2a92289
lucien1011 <lucien1011@gmail.com>
2020-01-18 06:15:05 -0500

    record

---
+++ manage_crab.py
@@ -0,0 +1,18 @@
+import os,copy,math,argparse,glob
+from Utils.mkdir_p import mkdir_p
+from CRABAPI.RawCommand import crabCommand
+
+parser = argparse.ArgumentParser()
+#parser.add_argument("--inputDir",action="store")
+parser.add_argument("--outputDir",action="store")
+parser.add_argument("--pattern",action="store")
+parser.add_argument("--dry_run",action="store_true")
+parser.add_argument("--verbose",action="store_true")
+
+option = parser.parse_args()
+
+for crabDir in glob.glob(option.pattern):
+    if option.verbose:
+        print "********************"
+        print "Running on directory "+crabDir
+    res = crabCommand('status',dir=crabDir)
